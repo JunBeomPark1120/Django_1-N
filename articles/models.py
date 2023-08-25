267ef6f5fabee67e14219e9ba9b08360e7ed8585
junbeompark <a01077348062@gmail.com>
2023-08-25 11:07:12 +0900

    create 구현

--- articles/models.py
+++ articles/models.py
@@ -8,3 +8,7 @@ class Article(models.Model):
 class Comment(models.Model):
     content = models.TextField()
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    
+    # 용어 설명
+    # pk : 기본키라 하여, 고유한 값 설정하는 키
+    # fk : 외래키라 하여, 나의 고유한 값은 아니지만 M:N 관계에서 자주 쓰이는 키
